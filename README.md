7ecbfa94a5a2c293c43441240520b776c83b5686
rekhar <rekhar@qburst.com>
2016-12-21 13:25:50 +0530

    architecture diagram for Fe structure

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Architecture-Representation
+
+This is a graphical representation of frontend architecture of projects and is done using d3.js. This will give an clear idea of the FE structutre with a simple graph.
+
+The sample project is having a component based structure which mainly consist fo two modules "Login" and "Main" which is made up of soem components. 
+
+For example, the "Login" module consist of the components "header", "errorbox", "services", and "footer". Each of these components will contain an html file, module definition file, component file, controller, css, and service file if any.
---
+++ data.js
@@ -0,0 +1,95 @@
+var treeData = [{
+    "name": "App",
+    "stroke": "rgb(245, 237, 48)",
+    "parent": "null",
+    "children": [{
+        "name": "Login",
+        "stroke": "rgb(91, 167, 23)",
+        "children": [{
+            "name": "header",
+            "stroke": "rgb(228, 137, 23)"
+        }, {
+            "name": "services",
+            "stroke": "rgb(228, 137, 23)"
+        }, {
+            "name": "errorbox",
+            "stroke": "rgb(228, 137, 23)"
+        }, {
+            "name": "footer",
+            "stroke": "rgb(228, 137, 23)"
+        }]
+    }, {
+        "name": "Main",
+        "stroke": "rgb(91, 167, 23)",
+        "children": [{
+            "name": "Home",
+            "stroke": "rgb(91, 167, 23)",
+            "children": [{
+                "name": "header",
+                "stroke": "rgb(228, 137, 23)"
+            }, {
+                "name": "services",
+                "stroke": "rgb(228, 137, 23)"
+            }, {
+                "name": "statusbox",
+                "stroke": "rgb(228, 137, 23)"
+            }, {
+                "name": "uploader",
+                "stroke": "rgb(228, 137, 23)"
+            }, {
+                "name": "errorbox",
+                "stroke": "rgb(228, 137, 23)",
+                "children": [{
+                    "name": "errorbox.html (The template/ partial file)",
+                    "stroke": "rgb(23, 171, 239)"
+                }, {
+                    "name": "errorbox.js (Module definition)",
+                    "stroke": "rgb(23, 171, 239)"
+                }, {
+                    "name": "errorbox.component.js (Defines the behavior and exporting as a directive)",
+                    "stroke": "rgb(23, 171, 239)"
+                }, {
+                    "name": "errorbox.controller.js (The controller file)",
+                    "stroke": "rgb(23, 171, 239)"
+                }, {
+                    "name": "errorbox.css (Styles associated if any)",
+                    "stroke": "rgb(23, 171, 239)"
+                }, {
+                    "name": "errorbox.service.js (Component specific service class if any)",
+                    "stroke": "rgb(23, 171, 239)"
+                }]
+            }, {
+                "name": "footer",
+                "stroke": "rgb(228, 137, 23)"
+            }]
+        }, {
+            "name": "Result",
+            "stroke": "rgb(91, 167, 23)",
+            "children": [{
+                "name": "header",
+                "stroke": "rgb(228, 137, 23)"
+            }, {
+                "name": "services",
+                "stroke": "rgb(228, 137, 23)"
+            }, {
+                "name": "chartbox",
+                "stroke": "rgb(228, 137, 23)"
+            }, {
+                "name": "datatable",
+                "stroke": "rgb(228, 137, 23)"
+            }, {
+                "name": "donutchart",
+                "stroke": "rgb(228, 137, 23)"
+            }, {
+                "name": "summary",
+                "stroke": "rgb(228, 137, 23)"
+            }, {
+                "name": "errorbox",
+                "stroke": "rgb(228, 137, 23)"
+            }, {
+                "name": "footer",
+                "stroke": "rgb(228, 137, 23)"
+            }]
+        }]
+    }]
+}];
